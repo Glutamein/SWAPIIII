a3e209d2a6f5090379dab8d735d5c1126dc6fdba
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-05-01 11:48:24 -0600

    SWAPIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

---
+++ swapi-new/script.js
@@ -0,0 +1,39 @@
+var data;
+var type;
+var id_num;
+var url;
+var step;
+var holder = [];
+var request;
+ 
+function findData() {
+  url = 'https://swapi.co/api/' + type + '/' + id_num;
+  step = 1;
+  loadData(url);
+}
+ 
+function loadData(url) {
+  request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onload = loadComplete;
+  request.send();
+}
+ 
+function loadComplete(evt) {
+  data = JSON.parse(request.responseText);
+  holder.push(data);
+  if(step == 1){
+    loadData(data.species);
+  }
+  if(step == 2){
+    loadData(data.homeworld);
+  }
+  if(step == 3){
+    console.log(holder);
+  }
+  step++;
+}
+
+type = 'people';
+id_num = Math.floor(Math.random()*89);
+findData();
