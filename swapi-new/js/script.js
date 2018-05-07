b165334d72cc1c1b301fe2097ccd6245592f9de1
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-05-06 23:21:34 -0600

    ch-ch-ch-ch changes

--- swapi-new/js/script.js
+++ swapi-new/js/script.js
@@ -5,40 +5,48 @@ var url;
 var step;
 var holder = [];
 var request;
- 
+
 function findData() {
-  url = 'https://swapi.co/api/' + type + '/' + id_num;
-  step = 1;
-  loadData(url);
-}
- 
-function loadData(url) {
-  request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.onload = loadComplete;
-  request.send();
-}
- 
-function loadComplete(evt) {
-  data = JSON.parse(request.responseText);
-  holder.push(data);
-  if(step == 1){
-    loadData(data.species);
-  }
-  if(step == 2){
-    loadData(data.homeworld);
-  }
-  if(step == 3){
-    console.log(holder);
-  }
-  step++;
+	url = 'https://swapi.co/api/' + type + '/' + id_num;
+	step = 1;
+	loadData(url);
 }
 
-function triviaHandler(){
-  var text = this.responceText;
-  console.log(JSON.parse(text));
+function person() {
+
+}
+
+function home() {
+	var home = document.getElementById()
+}
+
+function loadData(url) {
+	request = new XMLHttpRequest();
+	request.open('GET', url);
+	request.onload = loadComplete;
+	request.send();
+}
+
+function loadComplete(evt) {
+	data = JSON.parse(request.responseText);
+	holder.push(data);
+	if (step == 1) {
+		loadData(data.species);
+	}
+	if (step == 2) {
+		loadData(data.homeworld);
+	}
+	if (step == 3) {
+		console.log(holder);
+	}
+	step++;
+}
+
+function triviaHandler() {
+	var text = this.responceText;
+	console.log(JSON.parse(text));
 }
 
 type = 'people';
-id_num = Math.floor(Math.random()*89);
+id_num = Math.floor(Math.random() * 89);
 findData();
